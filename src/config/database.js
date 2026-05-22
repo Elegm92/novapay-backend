9fe25adee6069f5b11898d23aeaed6609c54046b
Elegm92 <elegm92@gmail.com>
2026-05-22 15:35:36 +0200

    feature:Add config de BBDD en supabase

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,15 @@
 import "dotenv/config";
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
-);
+  logging: false,
+});
 
 const connectDB = async () => {
   try {
